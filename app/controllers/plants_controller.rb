39581068ea41b865865d7df939cdb2c1ba689522
George Njogu <sbeveme@gmail.com>
2022-11-28 16:11:21 +0300

    assignment done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,27 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # UPDATE /plants/:id
+  def update 
+    plant = Plant.find_by(id:params[:id])
+    if plant
+      Plant.update(is_in_stock: plant.is_in_stock = false)
+      render json: plant, except: [:created_at, :updated_at]
+    else
+      render json: {error: "plant not found"}, status: :not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(params[:id])
+    if plant
+      plant.destroy()
+      head :no_content
+    else
+      render json: {error: "plant not found"}, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
